46eb10e691aefb26a1f83612f9aba5b8749775ff
manulzvz13 <manulzvz@gmail.com>
2025-08-08 09:06:22 -0600

    feat: Fetch products data via async await:

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const featchProducts = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/products");
+        if (!res.ok) throw new Error("Failed to fetch products");
+        const data = await res.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    featchProducts();
+  }, []);
+
   return <div>shopBuddy UI</div>;
 };
 
