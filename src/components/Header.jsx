e312c993dc0d2a94c664ae3df319d11d14c0e98f
manulzvz13 <manulzvz@gmail.com>
2025-08-09 13:03:39 -0600

    feat: Remove items from dropdown feature and clear cart button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { useCart } from "../context/CartContext";
 const Header = () => {
   const [showDropdown, setShowDropdown] = useState(false);
 
-  const { cart } = useCart();
+  const { cart, removeFromCart, clearCart } = useCart();
   const itemCount = cart.reduce((acc, item) => acc + item.qty, 0);
   const total = cart
     .reduce((acc, item) => acc + item.price * item.qty, 0)
@@ -45,6 +45,12 @@ const Header = () => {
                             {item.qty} x ${item.price}
                           </p>
                         </div>
+                        <button
+                          onClick={() => removeFromCart(item.id)}
+                          className="text-sm text-red-500 hover:underline"
+                        >
+                          Remove
+                        </button>
                       </li>
                     ))}
                   </ul>
@@ -52,6 +58,12 @@ const Header = () => {
                     <span>Total:</span>
                     <span>${total}</span>
                   </div>
+                  <button
+                    onClick={clearCart}
+                    className="mt-3 w-full bg-red-500 text-white py-1 rounded transition hover:bg-gray-600"
+                  >
+                    Clear Cart
+                  </button>
                 </>
               )}
             </div>
